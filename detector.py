f026bc163b4c270d1ff23e7dde91bd1b83696e71
okazawa <19aj023@ms.dendai.ac.jp>
2022-07-25 19:31:58 +0900

    Create API server

--- detector.py
+++ detector.py
@@ -1,6 +1,7 @@
 import os
 import sys
 from pathlib import Path
+import yaml
 
 FILE = Path(__file__).resolve()
 ROOT = FILE.parents[0]  # YOLOv5 root directory
@@ -21,36 +22,37 @@ from yolov5.utils.torch_utils import select_device
 DEVICE = 0
 WEIGHTS = ROOT / 'fashion/fashionpedia14/weights/best.pt'
 DATA_YAML = ROOT / 'datasets/dataset.yaml'
-DEVICE = 0
 IMG_SIZE = (1024, 1024)
 
 
 class FashionDetector:
-    """ファッションアイテムを検出するクラス
-
-    Attributes:
-        device: GPU
-        model: YOLOのモデル
-        stride:
-        names:
-        pt:
-        imgsz: 画像のサイズ
-    """
+    """ファッションアイテムを検出するクラス"""
     def __init__(self):
         # Load model
         self.device = select_device(DEVICE)
-        self.model = DetectMultiBackend(
-            WEIGHTS, device=self.device, data=DATA_YAML)
+        self.model = DetectMultiBackend(WEIGHTS, device=self.device, data=DATA_YAML)
 
         self.imgsz = check_img_size(IMG_SIZE, s=self.model.stride)
         self.model.warmup(imgsz=(1, 3, *self.imgsz))
+
+        self.set_categories(DATA_YAML)
+    
+    def set_categories(self, data_yaml):
+        """データセットについて記述したyamlファイルからカテゴリを読み込む
+        Args:
+            data_yaml(str): データセットについて記述したyamlファイルのパス
+        """
+        with open(data_yaml) as f:
+            data_config = yaml.safe_load(f)
+        self.num_category = data_config['nc']
+        self.category_names = data_config['names']
     
     def detect(self, image):
         """ファッションアイテムを検出する
         Args:
-            image(numpy.ndarray): 検出対象の画像
+            image(np.ndarray): 検出対象の画像
         Returns:
-            numpy.ndarray: (46, )の各カテゴリの確信度（２つ以上検出した場合は，最大値）
+            response(dict): 各カテゴリの確信度（２つ以上検出した場合は，最大値）
         """
         # Padded resize
         img = letterbox(image, self.imgsz, stride=self.model.stride, auto=self.model.pt)[0]
@@ -66,21 +68,20 @@ class FashionDetector:
         pred = self.model(img)
         preds = non_max_suppression(pred)[0]
 
-        # カテゴリごと（46カテゴリ）の確信度を保持する
-        response = np.zeros(46)
+        # カテゴリごとの確信度を保持する
+        coef_scores = [0. for _ in range(self.num_category)]
         for pred in preds:
             conf = pred[-2].cpu().item()
             category_id = int(pred[-1])
-            response[category_id] = max(response[category_id], conf)
+            coef_scores[category_id] = max(coef_scores[category_id], conf)
+        
+        response = dict(zip(self.category_names, coef_scores))
 
         return response
 
 
-
-
-
-import cv2
-img = cv2.imread('datasets/images/test/0a4f8205a3b58e70eec99fbbb9422d08.jpg')
-detector = FashionDetector()
-print(detector.detect(img))
-import pdb;pdb.set_trace()
+if __name__ == '__main__':
+    import cv2
+    img = cv2.imread('datasets/images/test/0cb2351c74ccc0b38128e91d0629625d.jpg')
+    detector = FashionDetector()
+    print(detector.detect(img))
